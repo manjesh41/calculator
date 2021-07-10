4c6e3de06b47e5c04775d2521b4a59f345e98edb
ACER <manjeshsilwal@gmail.com>
2021-07-10 21:17:59 +0545

    commit

--- main.py
+++ main.py
@@ -23,32 +23,55 @@ def button_clear():
 def button_add():
     first_number= e.get()
     global f_num
+    global math
+    math='addition'
     f_num = int(first_number)
     e.delete(0,END)
 def button_sub():
     first_number= e.get()
     global f_num
+    global math
+    math='subtraction'
     f_num = int(first_number)
     e.delete(0,END)
-def button_per():
-    first_number= e.get()
-    global f_num
-    f_num = int(first_number)
-    e.delete(0,END)
+
 def button_mul():
     first_number= e.get()
     global f_num
+    global math
+    math='multiplication'
     f_num = int(first_number)
     e.delete(0,END)
 def button_div():
     first_number= e.get()
     global f_num
+    global math
+    math='division'
+    f_num = int(first_number)
+    e.delete(0,END)
+def button_floor():
+    first_number= e.get()
+    global f_num
+    global math
+    math='floordivision'
     f_num = int(first_number)
     e.delete(0,END)
 def button_equal():
+
     second_number = e.get()
     e.delete(0,END)
-    e.insert(0,f_num % int(second_number))
+    if math=='addition':
+        e.insert(0,f_num + int(second_number))
+    elif math=='subtraction':
+        e.insert(0, f_num - int(second_number))
+    elif math=='multiplication':
+        e.insert(0, f_num * int(second_number))
+    elif math=='division':
+        e.insert(0, f_num / int(second_number))
+    elif math=='floordivision':
+        e.insert(0, f_num // int(second_number))
+
+
 
 
 
@@ -65,8 +88,9 @@ b10=Button(root,text='+',font='lucid 20 bold',fg='red',bg='black',command= butto
 b11=Button(root,text='0',font='lucid 20 bold',fg='white',bg='black',command=lambda :button_click(0))
 b12=Button(root,text='-',font='lucid 20 bold',fg='red',bg='black',command= button_sub)
 b13=Button(root,text='*',font='lucid 20 bold',fg='red',bg='black',command= button_mul)
+b14=Button(root,text='//',font='lucid 20 bold',fg='red',bg='black',command=button_floor)
 
-b14=Button(root,text='%',font='lucid 20 bold',fg='red',bg='black',command=button_per)
+
 b15=Button(root,text='=',font='lucid 20 bold',fg='red',bg='black',command=button_equal)
 b16=Button(root,text='/',font='lucid 20 bold',fg='red',bg='black',command= button_div)
 b17=Button(root,text='clear',font='lucid 20 bold',fg='red',bg='black',command=button_clear)
@@ -83,11 +107,12 @@ b9.grid(column=2,row=3,pady=10,padx=10,ipadx=20,ipady=7)
 b10.grid(column=0,row=4,pady=10,padx=10,ipadx=20,ipady=7)
 b11.grid(column=1,row=4,pady=10,padx=10,ipadx=20,ipady=7)
 b12.grid(column=2,row=4,pady=10,padx=10,ipadx=20,ipady=7)
-b13.grid(column=0,row=5,pady=10,padx=10,ipadx=20,ipady=7)
-b14.grid(column=1,row=5,pady=10,padx=10,ipadx=20,ipady=7)
+b13.grid(column=0,row=5,pady=10,padx=10,ipadx=30,ipady=7)
+b14.grid(column=1,row=5,pady=10,padx=10,ipadx=30,ipady=7)
+
 b15.grid(column=2,row=5,pady=10,padx=10,ipadx=20,ipady=7)
 b16.grid(column=0,row=6,pady=10,padx=10,ipadx=30 , ipady=7 )
-b17.grid(column=1,row=6,columnspan=2,pady=10,padx=10,ipadx=30,ipady=7)
+b17.grid(column=1,row=6,columnspan=3,pady=10,padx=10,ipadx=30,ipady=7)
 
 
 
